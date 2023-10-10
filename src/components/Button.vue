06d0191367441c4c4ea74bc5fe047b0a4025ffed
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-10-10 23:31:38 +0600

    component practice

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<template>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Click me
+        </button>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/DataFetchingComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {reactive, onBeforeMount, onMounted} from 'vue'
+const apiResponse = reactive([])
+
+onBeforeMount(() => {
+  console.log('before mount')
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+      apiResponse.push(json)
+    })
+})
+
+
+onMounted(() => {
+  console.log(apiResponse);
+})
+
+</script>
+
+<template>
+
+</template>   
